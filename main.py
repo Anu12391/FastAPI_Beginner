3998cd54659c41d6f38900e1333e8c914e1437f0
Anu <anuavr123@gmail.com>
2025-05-20 11:47:20 +0530

    added parameters to Blog Model

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ print("🟢 RUNNING THE CORRECT main.py FILE")
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
     return {'data': {'name': 'Anu'}}
+
 
 @app.get('/about')
 def about():
@@ -25,33 +27,36 @@ def unpublished():
 def show(blog_id: int):
     return {'data': blog_id}
 
+
 @app.get('/blog/{blog_id}/comments')
 def comments(blog_id: int):
     print("COMMENTS ROUTE HIT")
     return {'data': blog_id}
 
+
 @app.get('/justtest')
 def mytest():
-
     return {'data': 'hii'}
 
 
 @app.get('/blog/{blog_id}/comments/{comment_id}')
-def comments(blog_id: int,comment_id:int):
+def comments(blog_id: int, comment_id: int):
     print("COMMENTS ROUTE HIT")
-    return {'blog_id': blog_id,'comment_id':comment_id}
+    return {'blog_id': blog_id, 'comment_id': comment_id}
+
 
 @app.get('/simpledata')
 def get_simple_data():
-    return {'data': {'arrays':{'1','2'}}}
+    return {'data': {'arrays': {'1', '2'}}}
 
 
 @app.get('/queryblog')
-def query_blog_with_limit(published:bool,limit=20,sort:Optional[str]=None):
+def query_blog_with_limit(published: bool, limit=20, sort: Optional[str] = None):
     if published:
         return {'data': f'{limit} published blogs per api call'}
     else:
         return {'data': f'{limit} blogs per api call'}
+
 
 @app.post('/blogpost')
 def create_blog_post():
@@ -59,8 +64,11 @@ def create_blog_post():
 
 
 class Blog(BaseModel):
-    pass
+    title: str
+    body: str
+    published: Optional[bool]
+
 
 @app.post('/blogcreate')
-def create_blog_pydantic(request:Blog):
+def create_blog_pydantic(request: Blog):
     return {'data': 'Blog is created'}
