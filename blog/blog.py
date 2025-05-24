3c88ade205609b0b1073da2ccf2db17dbf86a030
Anu <anuavr123@gmail.com>
2025-05-24 18:21:01 +0530

    added pydantic model in a different file

--- blog/blog.py
+++ blog/blog.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+from blog.schemas import Blog
+
 app = FastAPI()
 
-
 @app.post('/blogschema')
-def create_blog():
-    return 'creating'
+def create_blog(request: Blog):
+    return request
