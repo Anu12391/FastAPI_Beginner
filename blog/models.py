4775f7ac06201a459e0a50cab4b83799d235b137
Anu <anuavr123@gmail.com>
2025-05-26 18:57:38 +0530

    added more imports

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from sqlalchemy import Integer, Column, String
 
 from database import BASE
+from . import schemas, models
+from database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 
 class Blog(BASE):
