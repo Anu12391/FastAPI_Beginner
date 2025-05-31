27e88ea9f9254315e8baecf03210562948cf4e18
Anu <anuavr123@gmail.com>
2025-05-31 11:16:17 +0530

    added exception for delete if blog with that id is not present

--- blog/main.py
+++ blog/main.py
@@ -46,10 +46,15 @@ def get_blog_by_id(id: int, response: Response, db: Session = Depends(get_db)):
 
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return 'done'
+    blog= db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with this {id} is not found")
+    else:
+        blog.delete(synchronize_session=False)
+        db.commit()
+        return 'done'
 
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
