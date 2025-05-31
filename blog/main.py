ddf079eceeb427c3430d331dd56a28994018c180
Anu <anuavr123@gmail.com>
2025-05-31 11:13:48 +0530

    added exception if blog with that id is not present

--- blog/main.py
+++ blog/main.py
@@ -54,6 +54,11 @@ def destroy(id, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update({'title':request.title, 'body':request.body})
-    db.commit()
-    return 'updated'
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with this {id} is not found")
+
+    else:
+        blog.update({'title': request.title, 'body': request.body})
+        db.commit()
+        return 'updated'
