6e1f5b614026e645c9ee0f2b15f669c925180c8b
Anu <anuavr123@gmail.com>
2025-06-03 12:28:59 +0530

    added tags for each api endpoint

--- blog/main.py
+++ blog/main.py
@@ -24,7 +24,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blogschema', status_code=status.HTTP_201_CREATED)
+@app.post('/blogschema', status_code=status.HTTP_201_CREATED,tags=['blog'])
 def create_blog(request: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -33,13 +33,13 @@ def create_blog(request: Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blogall', response_model=List[ShowBlog])
+@app.get('/blogall', response_model=List[ShowBlog],tags=['blog'])
 def get_all_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200, response_model=ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model=ShowBlog,tags=['blog'])
 def get_blog_by_id(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -49,7 +49,7 @@ def get_blog_by_id(id: int, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,tags=['blog'])
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -60,7 +60,7 @@ def destroy(id, db: Session = Depends(get_db)):
         return 'done'
 
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED,tags=['blog'])
 def update(id, request: Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -72,7 +72,7 @@ def update(id, request: Blog, db: Session = Depends(get_db)):
         return 'updated'
 
 
-@app.post('/createuser', status_code=status.HTTP_201_CREATED, response_model=ShowUser)
+@app.post('/createuser', status_code=status.HTTP_201_CREATED, response_model=ShowUser,tags=['user'])
 def create_user(request: User, db: Session = Depends(get_db)):
     hashed_password = Hash().bcrypt(request.password)
     new_user = models.User(name=request.name, email=request.email, password=hashed_password)
@@ -82,7 +82,7 @@ def create_user(request: User, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/getuser/{id}", status_code=status.HTTP_200_OK,response_model=ShowUser)
+@app.get("/getuser/{id}", status_code=status.HTTP_200_OK,response_model=ShowUser,tags=['user'])
 def getuser(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
