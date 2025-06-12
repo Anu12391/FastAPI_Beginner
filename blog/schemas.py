ada784a3e0159d8fed4af26c170ed247f4536195
Anu <anuavr123@gmail.com>
2025-06-12 12:57:00 +0530

    created token.py added secret key, alogrith and token pydantic models

--- blog/schemas.py
+++ blog/schemas.py
@@ -49,3 +49,12 @@ class Blog(BlogBase):
 class Login(BaseModel):
     username: str
     password:str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
--- routers/authentication.py
+++ routers/authentication.py
@@ -13,13 +13,13 @@ router = APIRouter(tags=["Authentication"], prefix="/user")
 
 @router.post('/login')
 def login(request: Login, db: Session = Depends(get_db)):
-    hashObj=Hash()
+    hashObj:Hash=Hash()
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
-    if hashObj.verify(user.password, request.password):
+    if not hashObj.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Invalid Credentials")
+                            detail=f"Invalid Password")
 
     return user
