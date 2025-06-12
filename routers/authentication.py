22fd02633b7c3686320d4bd4dfe0599c08941e39
Anu <anuavr123@gmail.com>
2025-06-12 13:48:51 +0530

    added access token for login endpoint

--- routers/authentication.py
+++ routers/authentication.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
@@ -6,7 +8,8 @@ from starlette import status
 from blog import database, models
 from blog.database import get_db
 from blog.hashing import Hash
-from blog.schemas import Login
+from blog.schemas import Login, Token
+from routers.token import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter(tags=["Authentication"], prefix="/user")
 
@@ -21,5 +24,9 @@ def login(request: Login, db: Session = Depends(get_db)):
     if not hashObj.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}
+    )
 
-    return user
+    return Token(access_token=access_token, token_type="bearer")
