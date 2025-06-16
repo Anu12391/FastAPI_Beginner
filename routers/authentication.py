00e46ea9c4d5462d34fa472c207bf0556a0e0451
Anu <anuavr123@gmail.com>
2025-06-16 19:10:32 +0530

    oauthpassword request---not working

--- routers/authentication.py
+++ routers/authentication.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -15,8 +16,9 @@ router = APIRouter(tags=["Authentication"], prefix="/user")
 
 
 @router.post('/login')
-def login(request: Login, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db)):
     hashObj:Hash=Hash()
+
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -25,8 +27,12 @@ def login(request: Login, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Password")
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+
     access_token = create_access_token(
         data={"sub": user.email}
     )
 
-    return Token(access_token=access_token, token_type="bearer")
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
